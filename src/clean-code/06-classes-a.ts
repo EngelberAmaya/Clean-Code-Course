be08eb466565ef0c186c001b1ba953a6c3e1d5b2
EngelberAmaya <engelberamaya@gmail.com>
2023-05-20 17:32:57 -0400

    Heritage - problem

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,21 +1,65 @@
 (() => {
 
+    // No aplicando el principio de responsabilidad única
+
     type Gender = 'M'|'F';
 
     class Person {
-        // public name: string;
-        // public gender: Gender;
-        // public birthdate: Date;
-
         constructor(
-            public name: string,
-            public gender: Gender,
+            public name: string, 
+            public gender: Gender, 
             public birthdate: Date
         ){}
     }
 
-    const newPerson = new Person('Engelber', 'M', new Date('1991-03-12'));
 
-    console.log({newPerson});
+    class User extends Person {
+        
+        public lastAccess: Date;
 
-})()
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ) {
+            super( name, gender, birthdate );
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder  : string,
+            email                  : string,
+            role                   : string,
+            name                   : string,
+            gender                 : Gender,
+            birthdate              : Date
+        ) {
+            super(email, role, name, gender, birthdate );
+        }
+    }
+
+
+    const userSettings = new UserSettings(
+        '/usr/home',
+        '/home',
+        'engelber@google.com',
+        'Admin',
+        'Engelber',
+        'M',
+        new Date('1991-03-12')
+    );
+
+    console.log({ userSettings });
+
+
+})();
